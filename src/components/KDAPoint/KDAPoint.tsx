9a4bed0f5bfdc737111c0ff560f021713412b4c4
parkoon <devparkoon@gmail.com>
2021-04-05 15:14:06 +0900

    feat: add KDAPoint component

---
+++ src/components/KDAPoint/KDAPoint.tsx
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components'
+import { FONT_SIZE } from './constants'
+
+type FontSizeKey = keyof typeof FONT_SIZE
+type KDAPoint = {
+  kill: number
+  death: number
+  assist: number
+  size?: FontSizeKey
+}
+function KDAPoint({ size = 'md', kill, death, assist }: KDAPoint) {
+  return (
+    <Wrapper size={size}>
+      <Point divider>{kill}</Point>
+      <DeathPoint divider>{death}</DeathPoint>
+      <Point>{assist}</Point>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div<{ size: FontSizeKey }>`
+  font-size: ${({ size }) => FONT_SIZE[size]};
+  font-weight: bold;
+`
+
+const Point = styled.span<{ divider?: boolean }>`
+  position: relative;
+
+  ${({ divider }) =>
+    divider &&
+    css`
+      &::after {
+        content: '/';
+        padding: 0 2px;
+        color: ${({ theme }) => theme.color.grey[3]};
+      }
+    `}
+`
+
+const DeathPoint = styled(Point)`
+  color: ${({ theme }) => theme.color.red};
+`
+
+export default KDAPoint
---
+++ src/components/KDAPoint/constants.ts
@@ -0,0 +1,6 @@
+const FONT_SIZE = {
+  sm: '11px',
+  md: '14px',
+}
+
+export { FONT_SIZE }
