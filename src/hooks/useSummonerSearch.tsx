0a2083a7e77c6e3a60432df6217d45a87a408d3f
parkoon <devparkoon@gmail.com>
2021-04-07 23:32:05 +0900

    feat: search

---
+++ src/hooks/useSummonerSearch.tsx
@@ -0,0 +1,28 @@
+import { AxiosResponse } from 'axios'
+import { useState } from 'react'
+import useSWR from 'swr'
+import fetcher from '../lib/fetcher'
+import { Summoner } from '../model/Summoner'
+
+export const useSummonerSearch = () => {
+  const [summonerName, setSummonerName] = useState('')
+
+  const { data, error } = useSWR<AxiosResponse<{ summoner: Summoner }>>(
+    summonerName ? `/api/summoner/${summonerName}` : null,
+    fetcher,
+    { revalidateOnMount: false, revalidateOnFocus: false }
+  )
+
+  const initialLoading = !data && !error
+
+  const summoner = data?.data.summoner || ({} as Summoner)
+
+  const hasResult = Object.keys(summoner).length > 0
+
+  return {
+    summoner,
+    hasResult,
+    initialLoading,
+    setSummonerName,
+  }
+}
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,10 +1,16 @@
-import { KeyboardEvent } from 'react'
+import { lighten } from 'polished'
+import { ChangeEvent, KeyboardEvent } from 'react'
 import { useHistory } from 'react-router'
 import styled from 'styled-components'
+import ChampionProfile from '../../components/ChampionProfile'
 import Search from '../../components/Search'
+import { useSummonerSearch } from '../../hooks/useSummonerSearch'
 
 function Header() {
   const history = useHistory()
+
+  const { setSummonerName, summoner, hasResult, initialLoading } = useSummonerSearch()
+
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.keyCode !== 13) return
 
@@ -15,10 +21,46 @@ function Header() {
       search: `?username=${target.value}`,
     })
   }
+
+  const handleChange = (e: ChangeEvent) => {
+    const target = e.target as HTMLInputElement
+    setSummonerName(target.value)
+  }
+
+  const handleSummonerClick = (name: string) => {
+    setSummonerName('')
+    history.push({
+      pathname: '/',
+      search: `?username=${name}`,
+    })
+  }
+
   return (
     <Wrapper>
       <div />
-      <Search placeholder="소환사명, 챔피언..." onKeyDown={handleKeyDown} />
+
+      <SearchWrapper>
+        <Search
+          placeholder="소환사명, 챔피언..."
+          onKeyDown={handleKeyDown}
+          onChange={handleChange}
+        />
+        {!initialLoading && hasResult && (
+          <SummonerList>
+            <SummonerItem onClick={() => handleSummonerClick(summoner.name)}>
+              <ChampionProfile
+                title={summoner.name}
+                subtitle={
+                  summoner.previousTiers
+                    ? summoner.previousTiers[0].string
+                    : `Level ${summoner.level}`
+                }
+                src={summoner.profileImageUrl}
+              />
+            </SummonerItem>
+          </SummonerList>
+        )}
+      </SearchWrapper>
     </Wrapper>
   )
 }
@@ -31,6 +73,29 @@ const Wrapper = styled.header`
   padding: 12px 32px;
   background: ${({ theme }) => theme.color.header};
   min-width: 1000px;
+`
+const SearchWrapper = styled.div`
+  position: relative; ;
+`
+
+const SummonerList = styled.ul`
+  position: absolute;
+  width: 100%;
+  border-radius: 2px;
+  background-color: ${({ theme }) => theme.color.white};
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 50%);
+  z-index: 999;
+  top: 35px;
+  padding-top: 10px;
+`
+
+const SummonerItem = styled.li`
+  cursor: pointer;
+  transition: 0.2s;
+  padding: 4px;
+  &:hover {
+    background: ${({ theme }) => lighten(0.4, theme.color.blue)};
+  }
 `
 
 export default Header
