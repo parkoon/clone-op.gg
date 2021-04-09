3f5e8f1c46e30ff7c23011576b6b3b68e851249d
parkoon <devparkoon@gmail.com>
2021-04-09 22:06:25 +0900

    chore: remove comment

--- src/components/LeagueSection/LeagueSection.tsx
+++ src/components/LeagueSection/LeagueSection.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useSummonerInfo } from '../../hooks/fetch/useSummonerInfo'
 import { LeagueSectionLoading } from '../Loading'
 import LeagueCard from './LeagueCard'
-// import UnLeagueCard from '../LeagueCard/UnLeagueCard'
 
 function LeagueSection() {
   const { initialLoading, summoner } = useSummonerInfo()
@@ -16,9 +15,6 @@ function LeagueSection() {
       {leagues.map((league, index) => (
         <LeagueCard key={index} {...league} />
       ))}
-
-      {/* TODO: unranked? */}
-      {/* <UnLeagueCard wrapperStyle={{ marginBottom: 8 }} /> */}
     </article>
   )
 }
